57b065c4fe82a22932fbfc07d414d3c7f1f7e868
tomas <tomas>
2006-01-13 18:13:15 +0000

    Correcting copyright dates.

--- src/stable.lua
+++ src/stable.lua
@@ -1,8 +1,8 @@
 ----------------------------------------------------------------------------
 -- Stable: State persistent table.
 --
--- Copyright (c) 2004-2005 Kepler Project
--- $Id: stable.lua,v 1.1 2006/01/13 17:58:30 tomas Exp $
+-- Copyright (c) 2004-2006 Kepler Project
+-- $Id: stable.lua,v 1.2 2006/01/13 18:13:15 tomas Exp $
 ----------------------------------------------------------------------------
 
 local dostring = assert (dostring, "There is no `dostring'.  Probably not in a slave state")
@@ -12,7 +12,7 @@ assert (dostring[[persistent_table = persistent_table or {}]])
 module"stable"
 
 ----------------------------------------------------------------------------
-_COPYRIGHT = "Copyright (C) 2003-2006 Kepler Project"
+_COPYRIGHT = "Copyright (C) 2004-2006 Kepler Project"
 _DESCRIPTION = "State persistent table"
 _VERSION = "Stable 1.0"
 
